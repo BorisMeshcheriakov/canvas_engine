db2c1ef3486a9fa4e7f2740459d0614e0a36bf59
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2022-08-11 17:54:59 +0300

    add(canvas): animation loop

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,10 +1,35 @@
 import React from "react";
 
 const useCanvas = () => {
-  const canvasRef = React.useRef(null);
+  const canvasRef = React.useRef<null | HTMLCanvasElement>(null);
 
-  console.log(canvasRef);
+  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
 
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+    let frameCount = 0;
+    let animationFrameId = 0;
+
+    const render = () => {
+      frameCount++;
+      draw(context!, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  }, [draw]);
+
+  console.log(canvasRef.current);
   return { ref: canvasRef };
 };
 
